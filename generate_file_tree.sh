2ecd1fe1a48cb511a97bb04f07d5c9648283c2d4
Rapid User <rapiduser@rapid-1372.vm.duke.edu>
2020-10-27 21:22:17 -0400

    updated generate_file_tree.sh

--- generate_file_tree.sh
+++ generate_file_tree.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if ! pwd | grep -e 'dew-lover$'; then
+    echo 'please cd into the root of the dew-lover directory then run this script.'
+    exit
+fi
+
 if ! tree; then
     sudo apt-get install tree -y
 fi
