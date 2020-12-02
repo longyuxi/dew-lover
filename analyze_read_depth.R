9c0877c2d748513cffb49d15738e08826e7f0ee9
Yuxi Long <longyuxi@live.com>
2020-12-02 10:14:22 -0500

    downloaded idba

--- analyze_read_depth.R
+++ analyze_read_depth.R
@@ -5,6 +5,8 @@ library(ggplot2)
 library(plyr)
 coverage=rename(coverage,c(V1="Chr", V2="locus", V3="depth")) # renames the header
 
+depth_dist <- count(coverage, "depth")
+
 # Counts the number of bases where the read depth is smaller than 60
 ct <- 0
 for (row in 1:nrow(depth_dist)){
@@ -13,3 +15,11 @@ for (row in 1:nrow(depth_dist)){
   }
 }
 ct
+
+ggplot(depth_dist, aes(depth, freq)) +
+  geom_line() +
+  scale_x_log10()
+
+median(depth_dist$freq)
+
+# depth_dist: 28% < 60, average = 74, median = 20
